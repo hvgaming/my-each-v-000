b7611650d2438e1a06091805b320f8dbee82e54b
hugo valois <hvgaming@yahoo.com>
2018-09-05 03:00:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  new_array = []
+
+  for element in array
+    new_array.push yield element
+  end
+
+  new_array
+end
+
+my_each([1, 2, 3, 4) do |i| 
+  puts i
 end
